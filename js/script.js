e1517dec6b80b9b00ad4b55516611dcbb5f47314
SimplySka <skalyxe00@gmail.com>
2020-09-12 00:05:30 +0200

    js better

--- js/script.js
+++ js/script.js
@@ -8,6 +8,50 @@ function	age(birthday)
 	return (age);
 }
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+function	scrolling(pixel)
+{
+	window.scroll(0, pixel);
+}
+
+function	display(elem)
+{
+	elem.style.display = 'block';
+}
+
+window.onbeforeunload = function ()
+{
+	window.scrollTo(0, 0);
+}
+
+document.addEventListener("DOMContentLoaded", function(event)
+{ 
 	document.getElementById('age').innerHTML = age(new Date(1997, 05, 21));
+
+	document.getElementById('login').addEventListener("click", function()
+	{
+		i = 0;
+		slow = 1;
+		while (i < window.innerHeight)
+		{
+			slow += 0.1;
+			if (i > window.innerHeight * (3 / 4))
+				slow += 0.000001 * i;
+			else if (i < window.innerHeight * (1 / 4))
+				slow -= 0.000001 * (window.innerHeight - i);	
+			setTimeout(scrolling, (0.01 * slow) * i, i);
+			i++;
+		}
+		inputs = document.getElementsByClassName("input");
+		outputs = document.getElementsByClassName("output");
+		i = 0;
+		while (i < inputs.length)
+		{		
+			slow += 10;
+			setTimeout(display, (0.01 * slow) * window.innerHeight, inputs[i]);
+			setTimeout(display, ((0.01 * slow) * window.innerHeight) + 1, outputs[i]);
+			i++;
+		}
+	});
 });
+
+
