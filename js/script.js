59c56cafed6073cc7d413af649ba3c0bb55834a2
ska42 <skalyxe00@gmail.com>
2021-02-07 18:08:40 +0100

    better scroll & responsive

--- js/script.js
+++ js/script.js
@@ -18,10 +18,70 @@ function	display(elem)
 	elem.style.display = 'block';
 }
 
+function	hide(elem)
+{
+	elem.style.display = 'none';
+}
+
+function	print_char(div, c, timer)
+{
+	setTimeout(() => {div.append(c)}, timer);
+}
+
+
+function	print_input(div, input, slow)
+{
+	setTimeout(display, slow, div);
+	slow += 500;
+	let i = 0;
+	for (let i = 0; i < input.command.length; i++)
+	{
+		print_char(div, input.command[i], slow);
+		slow += 100;
+	}
+	if (input.args)
+	{
+		for (let i = 0; i < input.args.length; i++)
+		{
+			let node = document.createElement("h3");
+			setTimeout(() => {div.appendChild(node)}, slow);
+			slow += 100;
+			for (let j = 0; j < input.args[i].length; j++)
+			{
+				print_char(node, input.args[i][j], slow);
+				slow += 100;
+			}
+		}
+	}
+	return (slow);
+}
+
 window.onbeforeunload = function ()
 {
 	window.scrollTo(0, 0);
 }
+
+var input = [
+	{command: "pwd"},
+	{
+		command: "cat",
+		args: ["hello_world.txt"]
+	},
+	{
+		command: "cd",
+		args: ["Studies"]
+	},
+	{command: "ls -l"},
+	{
+		command: "cat",
+		args: [".working_environment.txt"]
+	},
+	{
+		command: "cd",
+		args: ["../Programming Language"]
+	},
+	{command: "ls -l"}
+];
 
 document.addEventListener("DOMContentLoaded", function(event)
 { 
@@ -29,8 +89,8 @@ document.addEventListener("DOMContentLoaded", function(event)
 
 	document.getElementById('login').addEventListener("click", function()
 	{
-		i = 0;
-		slow = 1;
+		let i = 0;
+		let slow = 1;
 		while (i < window.innerHeight)
 		{
 			slow += 0.1;
@@ -41,14 +101,16 @@ document.addEventListener("DOMContentLoaded", function(event)
 			setTimeout(scrolling, (0.01 * slow) * i, i);
 			i++;
 		}
-		inputs = document.getElementsByClassName("input");
-		outputs = document.getElementsByClassName("output");
+		slow = (0.01 * slow) * i; 
+		document.body.style.overflow = 'auto';
+		setTimeout(() => {hide(document.getElementById("header"))}, slow);
+		div_inputs = document.getElementsByClassName("input");
+		div_outputs = document.getElementsByClassName("output");
 		i = 0;
-		while (i < inputs.length)
+		while (i < div_inputs.length)
 		{		
-			slow += 10;
-			setTimeout(display, (0.01 * slow) * window.innerHeight, inputs[i]);
-			setTimeout(display, ((0.01 * slow) * window.innerHeight) + 1, outputs[i]);
+			slow = print_input(div_inputs[i], input[i], slow);
+			setTimeout(display, slow + 1, div_outputs[i]);
 			i++;
 		}
 	});
